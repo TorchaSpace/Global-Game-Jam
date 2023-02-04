e5e873243d60334efd912e28543e6396abfa9bc1
erdoganberke <erdogan.berke@metu.edu.tr>
2023-02-04 17:51:02 +0300

    Music System completed.

---
+++ Assets/Scripts/Music/Music.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> combatMusics;
+    [SerializeField] private List<AudioClip> mainMenuMusics;
+    [SerializeField] private List<AudioClip> creditsMusics;
+    [SerializeField] private float musicDelay;
+    private List<AudioClip> chosenMusics;
+    public enum musicType { combat, mainMenu, credits }
+    public musicType _musicType;
+    private AudioSource _audioSource;
+    private AudioClip _chosenMusic;
+    private int _currentMusicIndex;
+
+    private void OnEnable()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void MusicController()
+    {
+        ChooseMusic();
+        PlayMusic();
+    }
+   
+    private void ChooseMusic()
+    {
+        if (chosenMusics.Count == 1) 
+        {
+            _chosenMusic = chosenMusics[0];
+        }
+        else 
+        {
+            int random = Random.Range(0, chosenMusics.Count);
+            if (_currentMusicIndex != random)
+            {
+                _currentMusicIndex = random;
+                _chosenMusic = chosenMusics[_currentMusicIndex];
+            }
+            else
+            {
+                ChooseMusic();
+            }
+        }
+    }
+    private void PlayMusic()
+    {
+        _audioSource.clip = _chosenMusic;
+        _audioSource.Play();
+    }
+    public void ListChooser()
+    {
+        _audioSource.Stop();
+        StopCoroutine(Timer());
+        if (_musicType == musicType.combat)
+        {
+            chosenMusics = combatMusics;
+        }
+        else if (_musicType == musicType.mainMenu)   // switch case
+        {
+            chosenMusics = mainMenuMusics;
+        }
+        else if (_musicType == musicType.credits)
+        {
+            chosenMusics = creditsMusics;
+        }
+
+        StartCoroutine(Timer());
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _audioSource.Stop();                            // Deneme yeri
+            StopCoroutine(Timer());
+            StartCoroutine(Timer());
+        }
+    }
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(musicDelay);
+            MusicController();
+            yield return new WaitForSeconds(_audioSource.clip.length);
+
+        }
+    }
+}
