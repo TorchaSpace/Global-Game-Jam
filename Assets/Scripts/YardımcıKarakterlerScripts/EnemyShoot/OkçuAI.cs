4de8068650f226e6da48a88f4a339946b67d6ff8
TorchaSpace <ali.emre.celix@gmail.com>
2023-02-04 16:54:34 +0300

    Okçu

---
+++ Assets/Scripts/YardımcıKarakterlerScripts/EnemyShoot/OkçuAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OkçuAI : MonoBehaviour
+{
+    public float scanRadius = 3f;
+    public LayerMask filterMask;
+    private Collider2D checkCollider;
+
+    void Update()
+    {
+        checkCollider = Physics2D.OverlapCircle(transform.position, scanRadius, filterMask);
+
+        if (checkCollider != null && checkCollider.transform != transform)
+        {
+            Destroy(checkCollider.gameObject);
+        }
+    }
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, scanRadius);
+    }
+}
